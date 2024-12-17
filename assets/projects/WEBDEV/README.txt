cb1b998b354bb349d359d13e15f930fabb58ebb0
Severus <johnpaulalfaro05@gmail.com>
2024-12-17 22:36:43 +0800

    deploy

---
+++ assets/projects/WEBDEV/README.txt
@@ -0,0 +1,32 @@
+Daily Dripp Coffee Shop Bussiness Profile. (no database)
+
+*Unpack after downloading the zip file
+*Run in Visual Studio Code
+*Lastly, Enjoy!! 
+
+==CONTENT==
+- HOME
+- ABOUT US
+- MENU
+- GALLERY
+- CONTACT
+
+==Description==
+
+- On the header part, you will see the business logo and the navigation bar that contains Home, About us, Menu, Gallery, and Contact.
+
+- By clicking one of them, you will redirect to their specific page. 
+
+- On the Home page, you will see the business name and a simple slogan. There's also a button named "About us". By clicking it, you will redirect to the About us page.
+
+- As you scroll down, you will see the About us page. On this page, some information about the business is written. 
+
+- Next is the Menu page, there are different kinds of coffee blends in this page. By clicking a chosen flavor, a simple card will pop up with a image and a short description of the product and it's price. 
+
+- On the gallery page, there are pictures that are sliding automatically. 
+
+- On the contact page, you will see information about the business just like phone number and it's location. You can also get in touch with the owner by messaging them.
+
+- Last is the footer, you will see the Facebook and Instagram icons. By clicking them, you will redirect to their Facebook page and Instagram page.
+
+==END==
---
+++ assets/projects/WEBDEV/main.js
@@ -0,0 +1,88 @@
+function myNav() {
+    let bar = document.querySelector(".bar");
+    let nav = document.querySelector(".navigation");
+    bar.onclick = () => {
+        if (nav.style.left == "0%") {
+            nav.style.left = "-100%";
+        } else {
+            nav.style.left = "0%";
+
+
+        }
+    }
+}
+myNav();
+
+
+
+let preveiwContainer = document.querySelector('.products-preview');
+let previewBox = preveiwContainer.querySelectorAll('.preview');
+
+document.querySelectorAll('.products-container .product').forEach(product => {
+    product.onclick = () => {
+        preveiwContainer.style.display = 'flex';
+        let name = product.getAttribute('data-name');
+        previewBox.forEach(preview => {
+            let target = preview.getAttribute('data-target');
+            if (name == target) {
+                preview.classList.add('active');
+            }
+        });
+    };
+});
+
+previewBox.forEach(close => {
+    close.querySelector('.fa-times').onclick = () => {
+        close.classList.remove('active');
+        preveiwContainer.style.display = 'none';
+    };
+});
+
+
+var slideIndex =1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+
+}
+
+function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("dot");
+    if (n > slides.length ) {slideIndex =1}
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace("active", "");
+        }
+        slides[slideIndex - 1].style.display = "block";
+        dots[slideIndex -1].className += " active";
+}
+
+//auto slide
+
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > slides.length){
+        slideIndex = 1
+    }
+    slides [slideIndex - 1].style.display = "block";
+    setTimeout(showSlides, 2000);
+    // CHANGE IMAGE EVERY 2 SECOND
+}
